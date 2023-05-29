cdfe47dad7764f19389e72cf17eabf88488b022e
EdwardKabue <kabueedward@yahoo.com>
2023-05-29 20:27:35 +0300

    Simple math with numpy

--- numpyDifferentArrayTypes.py
+++ numpyDifferentArrayTypes.py
@@ -22,4 +22,31 @@ a = np.array([[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]])
 #print(np.random.random_sample(a.shape)) #generate matrix of random numbers with shape of existing matrix.
 
 #generate matrix with random integers.
-print(np.random.randint(7, size=(3,3)))
+#print(np.random.randint(7, size=(3,3)))
+
+#generate identity matrix
+#print(np.identity(4))
+
+#repeat an array.
+arr = np.array([[1,2,3]])
+r1 = np.repeat(arr, 3, axis=0)
+#print(r1)
+
+#modify array using other arrays.
+output = np.ones((5,5))
+#print(output)
+z = np.zeros((3,3))
+#print(z)
+z[1,1] = 9
+#print(z)
+
+output[1:4,1:4] = z
+#print(output)
+
+#make copy of existing array
+b = np.array([1,2,3])
+c = b #c and b point to the same array. Therefore, any change in c will reflect in the b reference.
+#print(c)
+c = b.copy() #make a copy of the array in b which is a new array unique to c.
+c[0] = 100
+#print(c)
---
+++ numpyMath.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+a = np.array([1,2,3,4])
+
+#add/substract a number to/from each element
+#print(a+2)
+#print(a-2)
+
+#multiply each element by a number
+#print(a*2)
+
+#divide each element by a number
+#print(a/2)
+
+#add 2 arrays
+b = np.array([1,0,1,0])
+#print(a+b)
+
+#square each element in the array
+#print(a**2)
+
+#get sin/cos of all the array elements
+#print(np.sin(a))
+print(np.cos(a))
